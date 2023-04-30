42f52efcfae5bc72a98eecb2e59b79c9a9a6841d
Simran Singh <9248519+ssinghjah@users.noreply.github.com>
2023-04-30 16:42:41 -0400

    Update encodeVideos.py

--- encodeVideos.py
+++ encodeVideos.py
@@ -1,14 +1,15 @@
 import os
+import ffmpeg
 
 # Define GoP
 GoP = 30
 
 # List of all source videos
-SourceFiles = ['Ven', 'Miami']
+SourceFiles = ['DetianFalls_4K']
 
 # List of QP values
 QPs = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-QPs = [0, 5]
+
 # List of metrics
 Metrics = []
 
@@ -16,21 +17,51 @@ GOP = 50
 
 SOURCE_FOLDER = './Source'
 ENCODED_FOLDER = './Encoded'
-RESOLUTION = '1920x1080'
+METADATA_FOLDER = './MetaData'
+FORMATs = ['.mp4', '.webm', '.yuv']
+
+
+def getResolutionFPS(probe):
+    resolution = ''
+    fps = ''
+    for stream in probe['streams']:
+        if stream['codec_type'] == 'video':
+            resolution = str(stream['width']) + 'x' + str(stream['height'])
+            fpsTerms = stream['r_frame_rate'].split('/')
+            fps = float(fpsTerms[0])/float(fpsTerms[1])
+            return resolution, str(fps)
+
+def getSourceFilePath(locationName):
+    candidatePath = ""
+    for vidFormat in FORMATs:
+        candidatePath = SOURCE_FOLDER + '/' + locationName + vidFormat
+        if(os.path.isfile(candidatePath)):
+            return candidatePath
 
 # Iterate over all source videos
-for sourceFile in SourceFiles:
+for locationName in SourceFiles:
+    print("-------------")
+    print(locationName)
+    yuvFile = SOURCE_FOLDER + '/' + locationName + '.yuv'
+    sourceFilePath = getSourceFilePath(locationName)
 
-    yuvFile = SOURCE_FOLDER + '/' + sourceFile + '.yuv'
+    probe = ffmpeg.probe(sourceFilePath)
+    resolution, fps = getResolutionFPS(probe)
+    print(resolution, fps)
+
     if (not os.path.isfile(yuvFile)):
         # convert to .yuv, if file does not exist
-        os.system('ffmpeg -i ' + SOURCE_FOLDER + '/' +  sourceFile + '.mp4 -c:v rawvideo -pixel_format 420p ' + yuvFile)
+        os.system('ffmpeg -i ' + sourceFilePath + ' -c:v rawvideo -pixel_format 420p ' + yuvFile)
+        
+    #os.system('ffmpeg -f rawvideo -vcodec rawvideo -s ' + resolution + '-r ' + fps +  '-pix_fmt yuv420p -i ' + yuvFile + '-c:v libx265 --tune zerolatency -qp 0 ' + SOURCE_FOLDER + '/' + locationName + '.mp4' )
+        
     for qp in QPs:
         # Re-encode at all QPs
-        os.system('x265 --input ' + SOURCE_FOLDER + '/' + sourceFile + '.yuv --output ' + ENCODED_FOLDER + '/' + sourceFile + '_' + str(qp) + '.mp4 --fps 30 --input-res ' + RESOLUTION +  ' --qp ' + str(qp) + ' --keyint ' + str(GOP) + ' --min-keyint '+ str(GOP))
-        # Calculate metric
-        os.system('ffmpeg -i ' + ENCODED_FOLDER + '/' +  sourceFile + '_' + str(qp)  + '.mp4 -i ' + SOURCE_FOLDER + '/' + sourceFile + '.mp4 -lavfi psnr=stats_file=' + ENCODED_FOLDER + '/' + sourceFile + '_psnr_' + str(qp)  + '.txt -f null -')
+        os.system('x265 --tune zerolatency  --input ' + SOURCE_FOLDER + '/' + locationName + '.yuv --output ' + ENCODED_FOLDER + '/' + locationName + '_' + str(qp) + '.mp4 --fps ' + str(fps) +  ' --input-res ' + resolution +  ' --qp ' + str(qp) + ' --keyint ' + str(GOP) + ' --min-keyint '+ str(GOP))
+
+        # Calculate PSNR metric
+        os.system('ffmpeg -i ' + ENCODED_FOLDER + '/' +  locationName + '_' + str(qp)  + '.mp4 -i ' + SOURCE_FOLDER + '/' + locationName + '.yuv -lavfi psnr=stats_file=' + METADATA_FOLDER + '/' + locationName + '_psnr_' + str(qp)  + '.txt -f null -')
     
         # display byte size of all frames
-        fPath = './Encoded/' + sourceFile + '_' + str(qp) + '.mp4'
-        os.system('ffprobe ' + fPath + ' -v error -select_streams V:0 -show_entries "frame=pict_type,pkt_size" -of csv=p=0 >frameInfo_' + sourceFile + '_' + str(qp) + '.csv')
+        fPath = ENCODED_FOLDER + '/' + locationName + '_' + str(qp) + '.mp4'
+        os.system('ffprobe ' + fPath + ' -v error -select_streams V:0 -show_entries "frame=pict_type,pkt_size" -of csv=p=0 >' + METADATA_FOLDER  + '/frameInfo_' + locationName + '_' + str(qp) + '.csv')
