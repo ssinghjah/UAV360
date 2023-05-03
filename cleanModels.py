625dd5af97addc069d2fc785f4501e3d593f3999
Simran <s.singh.xyz@gmail.com>
2023-05-03 19:45:11 -0400

    script to clean PSNR and Frame SIze models

--- cleanModels.py
+++ cleanModels.py
@@ -11,9 +11,9 @@ def writeCSVFile(csvFileName, data):
         writer.writerow(data)
 
 PSNR_INF = 250
-folder = './Encoded/'
-videoFNames = ['Miami', 'Ven']
-QPs = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+folder = './Results/MetaData/'
+videoFNames = ['Lucerne_4K_Trim', 'LakeShibara_8K_Trim']
+QPs = [10, 15, 20, 25, 30, 35, 40, 45, 50]
 for videoFName in videoFNames:
     qpMeans = []
     for QP in QPs:
@@ -23,14 +23,30 @@ for videoFName in videoFNames:
         with open(fPath) as f:
             lines = [line.rstrip() for line in f]
             for line in lines:
-                psnrReg = str(re.escape('psnr_avg:'))
-                emptySpaceReg = str(re.escape('psnr_y'))
+                psnrReg = str(re.escape('psnr_y:'))
+                emptySpaceReg = str(re.escape('psnr_u'))
                 psnr = float(re.findall(psnrReg+"(.*)" + emptySpaceReg, line)[0])
                 qpPSNRs.append(psnr)
                 #if not math.isinf(psnr):
                 #    qpPSNRs.append(psnr)
 
-            writeCSVFile(folder + videoFName + '_' + str(QP) + '.csv', qpPSNRs)
+            writeCSVFile(folder + videoFName + '_frameSNRs_QP_' + str(QP) + '.csv', qpPSNRs)
+
+
+for videoFName in videoFNames:
+    qpMeans = []
+    for QP in QPs:
+        fPath = folder + 'frameInfo_' + videoFName + '_' + str(QP) + '.csv'
+        qpFrameLens = []
+
+        # read file
+        with open(fPath) as f:
+            reader = csv.reader(fPath)
+            for line in enumerate(reader):
+                frameLen = float(line[0])
+                qpFrameLens.append(frameLen)
+            writeCSVFile(folder + videoFName + '_frameSizes_QP_' + str(QP) + '.csv', qpPSNRs)
+
         '''
         qpMeans.append(np.mean(qpPSNRs))
         csvFileName = folder + videoFName + '_PSNRs.csv'
